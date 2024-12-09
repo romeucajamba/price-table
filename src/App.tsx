784727b52e0713ecb00cf48808769eea4f190481
romeucajamba <romeucajamba@gmail.com>
2024-12-09 11:39:59 +0000

    feat: criado a tabela e inicializando o consumo de api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,91 @@
-
+import { Button } from "./components/ui/button"
+import { Input } from "./components/ui/input"
+import { Table, TableHead, TableHeader, TableBody, TableRow, TableCell } from "./components/ui/table";
+import { Search, PlusCircle } from "lucide-react";
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogDescription, DialogTitle } from "./components/ui/dialog";
+import { DialogFooter } from "./components/ui/dialog";
+import { Label } from "./components/ui/label";
+import { DialogClose } from "./components/ui/dialog";
+import { getProducts } from "./data/product";
+import { useQuery } from "@tanstack/react-query";
 
 export function App() {
+  const { data: products } = useQuery({
+    queryKey: ['products'],
+    queryFn:getProducts
+  })
 
   return (
-    <div className="bg-black">
-      <h1 className="text-blue-700 font-bold">Criação do site</h1>
+    <div className="p-6 max-w-4xl mx-auto space-y-4">
+      <h1 className="text-3xl font-bold">Produtos</h1>
+      
+      <div className="flex items-center justify-between">
+
+        <form className="flex items-center gap-2">
+          <Input name="id" placeholder="ID do pedido" className="w-auto"/>
+          <Input name="name" placeholder="Nome do produto" className="w-auto"/> 
+          <Button type="submit" variant="link">
+            <Search className="w-4 h-4 mr-2"/>
+            Filtrar resultados
+          </Button>
+        </form>
+          <Dialog>
+            <DialogTrigger>
+              <Button>
+                <PlusCircle className="w-4 h-4 mr-2"/>
+                Novo produto
+              </Button>
+            </DialogTrigger>
+
+            <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Produtos</DialogTitle>
+                  <DialogDescription>Adicionar produto no siatema</DialogDescription>
+                </DialogHeader>
+
+                <form className="space-y-6">
+
+                  <div className="grid grid-cols-4 items-center text-right gap-3">
+                    <Label htmlFor="name">Produto</Label>
+                    <Input id="name" className="col-span-3"/>
+                  </div>
+
+                  <div className="grid grid-cols-4 items-center text-right gap-3">
+                    <Label htmlFor="price">Preço</Label>
+                    <Input id="price" className="col-span-3"/>
+                  </div>
+
+                  <DialogFooter>
+                    <DialogClose asChild>
+                        <Button type="button" variant="destructive">Cancelar</Button>
+                      </DialogClose>
+                    <Button type="submit">Salvar</Button>
+                  </DialogFooter>
+                </form>
+            </DialogContent>
+          </Dialog>
+      </div>
+
+      <div className="border rounded-lg p-2">
+        <Table>
+          <TableHeader>
+            <TableHead>Id</TableHead>
+            <TableHead>Produto</TableHead>
+            <TableHead>Preço</TableHead>
+          </TableHeader>
+          <TableBody>
+            {products && products.map((product) => {
+              return(
+                <TableRow key={product.id}>
+                  <TableCell>{product.id}</TableCell>
+                  <TableCell>{product.name}</TableCell>
+                  <TableCell>{product.price.toLocaleString()}</TableCell>
+               </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   )
 }
---
+++ src/data/product.ts
@@ -0,0 +1,27 @@
+export async function getProducts() {
+    //delay de 1 seg
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    
+    return [
+        { id: 9881, name: "HgzAGHHz", price: 57.03 },
+        { id: 4342, name: "OrEEYKFb", price: 43.27 },
+        { id: 9336, name: "CszEQfkc", price: 49.31 },
+        { id: 8499, name: "irZRvaaH", price: 23.4 },
+        { id: 9770, name: "bznFaxWo", price: 57.3 },
+        { id: 5316, name: "wzBmOlzr", price: 51.43 },
+        { id: 5359, name: "YlQGHfsu", price: 67.68 },
+        { id: 9147, name: "dikzyeWv", price: 33.24 },
+        { id: 9045, name: "iaqUMlON", price: 33.03 },
+        { id: 4294, name: "CSSAXTze", price: 27.49 },
+        { id: 5753, name: "zbFsfFOa", price: 29.21 },
+        { id: 5114, name: "EhwnwIFK", price: 67.52 },
+        { id: 3673, name: "ybsiQvID", price: 54.11 },
+        { id: 6317, name: "zUclZxdW", price: 35.0 },
+        { id: 8218, name: "KkTUodiy", price: 60.85 },
+        { id: 6518, name: "OmNgTQPf", price: 51.42 },
+        { id: 2012, name: "xCwSYfzr", price: 37.64 },
+        { id: 2239, name: "tDqdpDCT", price: 63.25 },
+        { id: 4009, name: "sZdxpuSW", price: 67.61 },
+        { id: 4247, name: "uICcSRGT", price: 51.08 },
+      ];
+}
